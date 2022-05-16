61ecf3a65813796278994ae9b5f292e83a20d559
raja <rvnasaka23>
2022-05-16 13:43:30 +0530

    added spring initializr proj structure

---
+++ src/main/java/com/shoppy/blaze/BlazyApplication.java
@@ -0,0 +1,13 @@
+package com.shoppy.blaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlazyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppy/blaze/BlazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppy.blaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
